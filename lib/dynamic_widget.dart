aea689a8045cbdb1972fdce6f779a32cc01afb10
wdvqmj7377 <16353896+bhtri@users.noreply.github.com>
2021-01-18 17:24:09 +0900

    update code

--- lib/dynamic_widget.dart
+++ lib/dynamic_widget.dart
@@ -3,7 +3,7 @@ import 'package:flutter/material.dart';
 import 'package:http/http.dart' as http;
 
 class DynamicWidget extends StatelessWidget {
-  final url = "https://bhtri.github.io/jsonDesignExample/html/login.json";
+  final url = "https://bhtri.github.io/jsonDesignExample/html/list.json";
 
   @override
   Widget build(BuildContext context) {
